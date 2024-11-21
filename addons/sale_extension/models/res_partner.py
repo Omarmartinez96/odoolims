aa54b736423693d15455b532838f35935a4c4c52
Omar Martinez <omar.martinezqfb@gmail.com>
2024-11-20 17:07:31 -0800

    Adición de Código de cliente ren res_partner.py

--- addons/sale_extension/models/res_partner.py
+++ addons/sale_extension/models/res_partner.py
@@ -4,6 +4,12 @@ from odoo import models, fields
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
+    client_code = fields.Char(
+        string='Código de Cliente',
+        help='Código único para identificar al cliente',
+        index=True,  # Hacerlo buscable en la base de datos
+    )
+
     sucursal_id = fields.Many2one(
         'res.sucursal', string='Sucursal', 
         help='Sucursal a la que pertenece este contacto'
