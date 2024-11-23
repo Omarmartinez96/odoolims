693e3f666ddfe143a90089856c5c85176c1ad03f
Omar Martinez <omar.martinezqfb@gmail.com>
2024-11-22 16:35:55 -0800

    Correccion de bugs asociada a problemas con departamentos de clientes

--- addons/sale_extension/models/res_departamento.py
+++ addons/sale_extension/models/res_departamento.py
@@ -1,5 +1,5 @@
 # models/res_departamento.py
-from odoo import models, fields
+from odoo import models, fields, api
 
 class ResDepartamento(models.Model):
     _name = 'res.departamento'
@@ -8,13 +8,20 @@ class ResDepartamento(models.Model):
     name = fields.Char(string='Nombre del Departamento', required=True)
     sucursal_id = fields.Many2one(
         'res.sucursal', string='Sucursal', required=True, ondelete='cascade',
+        domain="[('cliente_id', '=', cliente_id)]",
         help='Sucursal a la que pertenece este departamento.'
     )
     cliente_id = fields.Many2one(
-        'res.partner', string='Cliente', related='sucursal_id.cliente_id', store=True,
+        'res.partner', string='Cliente', required=True, ondelete='cascade',
         help='Cliente relacionado a través de la sucursal.'
     )
     contactos_ids = fields.One2many(
         'res.partner', 'departamento_id', string='Contactos'
     )
     client_code = fields.Char(string='Código de Cliente', related='cliente_id.client_code', store=True)
+
+    @api.onchange('sucursal_id')
+    def _onchange_sucursal_id(self):
+        """Actualizar cliente automáticamente al seleccionar la sucursal."""
+        if self.sucursal_id:
+            self.cliente_id = self.sucursal_id.cliente_id
