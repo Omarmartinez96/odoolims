98d63dfc4000d42398df6cf99e7fb59c1487aa67
Omar Martinez <omar.martinezqfb@gmail.com>
2025-02-12 15:33:43 -0800

    Fix missing field rfc in lims.customer

--- addons/lims_customer/models/lims_customer.py
+++ addons/lims_customer/models/lims_customer.py
@@ -5,4 +5,6 @@ class LimsCustomer(models.Model):
     _description = 'Gestión de Clientes en LIMS'
 
     name = fields.Char(string="Nombre", required=True)
+    rfc = fields.Char(string="RFC")  # <--- Asegúrate de que esta línea esté
+    billing_partner_id = fields.Many2one('res.partner', string="Facturación")
     branch_ids = fields.One2many('lims.branch', 'customer_id', string="Sucursales")
