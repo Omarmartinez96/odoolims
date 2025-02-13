303cd8b93d14a918887519e86fad2baa94dbb144
Omar Martinez <omar.martinezqfb@gmail.com>
2025-02-12 18:06:40 -0800

    Cambio de nombres a modulos en dashboard

--- addons/lims_customer/models/lims_customer.py
+++ addons/lims_customer/models/lims_customer.py
@@ -5,7 +5,7 @@ class LimsCustomer(models.Model):
     _description = "Cliente del LIMS"
 
     name = fields.Char(string="Nombre del Cliente", required=True)
-    client_code = fields.Char(string="Código de Cliente", required=False)
+    client_code = fields.Char(string="Código de Cliente")
     fiscal_address = fields.Text(string="Dirección Fiscal")
 
     branch_ids = fields.One2many('lims.branch', 'customer_id', string="Sucursales")
