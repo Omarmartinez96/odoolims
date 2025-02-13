ec33525e908fd464381ed7d1922a19d7495ce286
Omar Martinez <omar.martinezqfb@gmail.com>
2025-02-12 18:09:20 -0800

    Cambio de nombres a modulos en dashboard

--- addons/lims_customer/models/lims_department.py
+++ addons/lims_customer/models/lims_department.py
@@ -1,9 +1,10 @@
 from odoo import models, fields
 
 class LimsDepartment(models.Model):
-    _name = "lims.department"
-    _description = "Departamento de la Sucursal"
+    _name = 'lims.department'
+    _description = 'Departamentos de la Sucursal'
 
     name = fields.Char(string="Nombre del Departamento", required=True)
-    branch_id = fields.Many2one('lims.branch', string="Sucursal", required=True)
+    branch_id = fields.Many2one('lims.branch', string="Sucursal", required=True, ondelete="cascade")
+    customer_id = fields.Many2one('lims.customer', string="Cliente", required=True, ondelete="cascade")
     contact_ids = fields.One2many('lims.contact', 'department_id', string="Contactos")
