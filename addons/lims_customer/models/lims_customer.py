6603f032b3115b737b82c30b9bbdcc4f0756b927
Omar Martinez <omar.martinezqfb@gmail.com>
2025-02-20 11:23:43 -0800

    Se a;ade logica de orden de codigo de clientes

--- addons/lims_customer/models/lims_customer.py
+++ addons/lims_customer/models/lims_customer.py
@@ -17,16 +17,17 @@ class LimsCustomer(models.Model):
         string="Sucursales"
     )
 
-    @api.model
-    def create(self, vals):
+    @api.model_create_multi
+    def create(self, vals_list):
         """Genera el Código del Cliente basado en el RFC y número consecutivo"""
-        if 'rfc' in vals and not vals.get('client_code'):
-            prefix = vals['rfc'][:3].upper() if vals['rfc'] else 'XXX'
-            existing_clients = self.search([('client_code', 'ilike', f'{prefix}-%')], order='client_code desc', limit=1)
-            if existing_clients:
-                last_number = int(existing_clients.client_code.split('-')[1])
-            else:
-                last_number = 0
-            new_code = f"{prefix}-{str(last_number + 1).zfill(3)}"
-            vals['client_code'] = new_code
-        return super(LimsCustomer, self).create(vals)
+        for vals in vals_list:
+            if 'rfc' in vals and not vals.get('client_code'):
+                prefix = vals['rfc'][:3].upper() if vals['rfc'] else 'XXX'
+                existing_clients = self.search([('client_code', 'ilike', f'{prefix}-%')], order='client_code desc', limit=1)
+                if existing_clients:
+                    last_number = int(existing_clients.client_code.split('-')[1])
+                else:
+                    last_number = 0
+                new_code = f"{prefix}-{str(last_number + 1).zfill(3)}"
+                vals['client_code'] = new_code
+        return super(LimsCustomer, self).create(vals_list)
