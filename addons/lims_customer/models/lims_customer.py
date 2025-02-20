0f1b25ae1a31349df08f14821bfc2c54c88ae728
Omar Martinez <omar.martinezqfb@gmail.com>
2025-02-20 10:58:04 -0800

    Se agrega RFC al campo de clientes

--- addons/lims_customer/models/lims_customer.py
+++ addons/lims_customer/models/lims_customer.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields
+from odoo import models, fields, api
 
 class LimsCustomer(models.Model):
     _name = 'lims.customer'
     _description = 'Clientes'
 
     name = fields.Char(string="Nombre del Cliente", required=True)
-    rfc = fields.Char(string="RFC")
+    rfc = fields.Char(string="RFC")  # ✅ Campo RFC agregado
     client_code = fields.Char(string="Código del Cliente")
     fiscal_address = fields.Char(string="Dirección Fiscal")
 
     # Relación con Sucursales (Una lista de sucursales para cada cliente)
     branch_ids = fields.One2many(
-        'lims.branch',  # modelo hijo
-        'customer_id',   # campo Many2one en el modelo hijo
+        'lims.branch',   # Modelo hijo
+        'customer_id',   # Campo Many2one en el modelo hijo
         string="Sucursales"
     )
 
-     @api.onchange('rfc')
+    @api.onchange('rfc')
     def _onchange_rfc(self):
         """Genera el Código del Cliente basado en los primeros 3 caracteres del RFC"""
         for record in self:
