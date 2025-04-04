c6ce2e2c54ad6db023d5866af192def9c231d703
Omar Martinez <omar.martinezqfb@gmail.com>
2025-04-03 19:01:21 -0700

    Ajuste: Se agregan nuevos campos a informacion general de CDC - Ajuste de manifest

--- addons/lims_reception/models/lims_sample.py
+++ addons/lims_reception/models/lims_sample.py
@@ -44,7 +44,7 @@ class LimsSample(models.Model):
         string="Fecha de Recepción", 
         default=fields.Datetime.now
     )
-    state = fields.Selection(
+    sample_state = fields.Selection(
         [('draft', 'Borrador'), ('in_analysis', 'En Análisis'), ('done', 'Finalizado')],
         default='draft'
     )
