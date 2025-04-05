905205205627b93fa3fbe301479b7800872584f2
Omar Martinez <omar.martinezqfb@gmail.com>
2025-04-04 19:07:27 -0700

    Ajuste: Se agrega envio de comprobante de CDC

--- addons/lims_reception/models/lims_custody_chain.py
+++ addons/lims_reception/models/lims_custody_chain.py
@@ -7,8 +7,10 @@ class LimsCustodyChain(models.Model):
     _description = 'Cadena de Custodia'
     _rec_name = 'custody_chain_code'
 
-    contact_id = fields.Many2one('res.partner',
-        string='Contacto'
+    contact_ids = fields.Many2many(
+        'lims.contact',
+        string='Contactos Relacionados',
+        domain="[('department_id', '=', departamento_id)]"
     )
     custody_chain_code = fields.Char(
         string="Código de Cadena de Custodia", 
@@ -72,21 +74,28 @@ class LimsCustodyChain(models.Model):
             if record.chain_of_custody_state != 'done':
                 raise UserError(_('La cadena de custodia debe estar finalizada para enviar el comprobante.'))
 
-            #Usar el contacto si existe, sino el cliente
-            email = record.contact_id.email or record.cliente_id.email
-            if not email:
-                raise UserError(_('No se encontró un correo electrónico válido en el contacto o cliente'))
+            # Obtener correos de contactos seleccionados
+            emails = [c.email for c in record.contact_ids if c.email]
 
-            #Busca la plantilla de correo
-            template = self.env.ref('lims_custody_chain.email_template_comprobante', 
-            raise_if_not_found=False
+            # Si no hay contactos o correos, usar el del cliente como respaldo
+            if not emails and record.cliente_id.email:
+                emails = [record.cliente_id.email]
+
+            if not emails:
+                raise UserError(_('No se encontró un correo electrónico válido en los contactos o cliente.'))
+
+            # Busca la plantilla de correo
+            template = self.env.ref(
+                'lims_custody_chain.email_template_comprobante', 
+                raise_if_not_found=False
             )
-            if not template: 
+            if not template:
                 raise UserError(_('No se encontró la plantilla de correo electrónico.'))
 
-            #Reemplazar dinámicamente el destinatario si es necesario
-            mail_values = {'email_to': email}
-            template.send_mail(record.id, 
-            force_send=True,
-            email_values=mail_values
+            # Enviar a múltiples correos (separados por coma)
+            mail_values = {'email_to': ",".join(emails)}
+            template.send_mail(
+                record.id, 
+                force_send=True, 
+                email_values=mail_values
             )
