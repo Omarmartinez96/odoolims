26eeac5cde1a487e20beaa07ec9f4447c5b3c772
Omar Martinez <omar.martinez92@gmail.com>
2025-05-08 13:52:39 -0700

    Fix: correct report attachment fields in email template

--- addons/lims_reception/models/lims_custody_chain.py
+++ addons/lims_reception/models/lims_custody_chain.py
@@ -82,7 +82,7 @@ class LimsCustodyChain(models.Model):
 
         # --- Dynamically generate the PDF report and attach it ---
         report = self.env.ref('lims_reception.action_report_custody_chain')
-        pdf_content, content_type = report._render_qweb_pdf(self.ids)
+        pdf_content, content_type = report._render_qweb_pdf(self.id)
 
         # Generate filename safely
         filename = '%s.pdf' % (self.custody_chain_code.replace('/', '_') if self.custody_chain_code else 'comprobante')
