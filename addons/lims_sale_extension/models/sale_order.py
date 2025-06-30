20b18af5420aa6e8f745dcbbcb4059f2c8062549
Omar Martinez <omar.martinez92@gmail.com>
2025-06-29 18:08:21 -0700

    Added sale module extension

--- addons/lims_sale_extension/models/sale_order.py
+++ addons/lims_sale_extension/models/sale_order.py
@@ -3,9 +3,16 @@ from odoo import models, fields
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    client_code = fields.Char(
+        string="Código del Cliente",
+        related='partner_id.client_code',
+        readonly=True,
+        store=True
+    )
+
     lims_branch_id = fields.Many2one(
         'lims.branch', 
-        string="Sucursal LIMS",
+        string="Sucursal",
         domain="[('customer_id', '=', partner_id)]"
     )
 
@@ -15,8 +22,11 @@ class SaleOrder(models.Model):
         domain="[('branch_id', '=', lims_branch_id)]"
     )
 
-    lims_contact_id = fields.Many2one(
+    lims_contact_ids = fields.Many2many(
         'lims.contact',
-        string="Contacto",
+        'sale_order_lims_contact_rel',  # tabla relacional
+        'order_id',                     # campo en sale.order
+        'contact_id',                   # campo en lims.contact
+        string="Contactos",
         domain="[('department_id', '=', lims_department_id)]"
     )
