91003c5b90e5b14c05efa0ace9a18dbcf3d74fd0
Omar Martinez <omar.martinez92@gmail.com>
2025-06-30 18:58:03 -0700

    Correccion de Bugs 17

--- addons/lims_sale_extension/__manifest__.py
+++ addons/lims_sale_extension/__manifest__.py
@@ -10,6 +10,7 @@
         'views/sale_order_views.xml',
         'views/report_quote.xml',
         'views/report.xml',
+        'views/external_layout_inherit.xml',
     ],
     'installable': True,
     'application': True,
