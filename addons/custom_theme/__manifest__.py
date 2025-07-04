331c6a45c5f6b9a28e6137ab532dc4f5d2d2ca17
Omar Martinez <omar.martinez92@gmail.com>
2025-07-03 17:26:00 -0700

    Intento de modificacion diseno desde backend 4

--- addons/custom_theme/__manifest__.py
+++ addons/custom_theme/__manifest__.py
@@ -6,15 +6,24 @@
     'author': 'Omar Martinez',
     'depends': ['web'],
     'assets': {
+        # Sobrescribimos variables en el momento correcto
+        'web._assets_primary_variables': [
+            'custom_theme/static/src/scss/custom_variables.scss',
+        ],
+        # Añadimos estilos personalizados al frontend y backend
         'web.assets_backend': [
             'custom_theme/static/src/scss/custom_variables.scss',
         ],
         'web.assets_frontend': [
             'custom_theme/static/src/scss/custom_variables.scss',
         ],
+        # Asegurar bootstrap mixins y funciones para darken()/lighten()
+        'web._assets_helpers': [
+            # ruta interna, no es obligatorio repetir si ya carga helpers
+        ],
     },
     'installable': True,
     'application': True,
-    'auto_install': True,
+    'auto_install': False,
     'license': 'LGPL-3',
 }
