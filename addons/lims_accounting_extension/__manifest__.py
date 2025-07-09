c9dd49d6e5444310686565480b2d3d415fb5991f
Omar Martinez <omar.martinez92@gmail.com>
2025-07-09 16:39:33 -0700

    Nuevo modulo de contabilidad para facturas personalizadas

---
+++ addons/lims_accounting_extension/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'LIMS Accounting Extension',
+    'version': '1.0',
+    'summary': 'Plantillas de factura LIMS personalizadas',
+    'category': 'Accounting',
+    'author': 'Proteus LIMS',
+    'depends': ['account', 'lims_sale_extension'],
+    'data': [
+        'views/external_layout_invoice_lims.xml',
+        'views/report_invoice_custom.xml',
+        'views/report.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
