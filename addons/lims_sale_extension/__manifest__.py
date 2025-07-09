a5b0d3d9c8a97f5dfa917597af0e08aabe59aa16
Omar Martinez <omar.martinez92@gmail.com>
2025-07-09 15:54:45 -0700

    Ajuste de tipos de Hoja 2

--- addons/lims_sale_extension/__manifest__.py
+++ addons/lims_sale_extension/__manifest__.py
@@ -9,6 +9,7 @@
     ],
     'data': [
         'views/external_layout_inherit_lims.xml',
+        'views/external_layout_invoice_folder.xml',
         'data/sale_order_sequence.xml',
         'views/sale_order_views.xml',
         'views/report_quote.xml',
