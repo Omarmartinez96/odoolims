5bb952d7deb1a64498a6e57b73258737aac88c27
Omar Martinez <omar.martinez92@gmail.com>
2025-07-12 11:47:53 -0700

    Ajuste a quote con html container 4

--- addons/lims_sale_extension/__manifest__.py
+++ addons/lims_sale_extension/__manifest__.py
@@ -8,7 +8,6 @@
         'web',
     ],
     'data': [
-        'views/external_layout_quote.xml',
         'data/sale_order_sequence.xml',
         'views/sale_order_views.xml',
         'views/report_quote.xml',
