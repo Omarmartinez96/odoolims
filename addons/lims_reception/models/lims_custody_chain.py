b62ef13b9c022b183e0bd3e2a3da99b13e4128a3
Omar Martinez <omar.martinez92@gmail.com>
2025-07-21 15:53:55 -0700

    Se agrega firma de validacion debug 5

--- addons/lims_reception/models/lims_custody_chain.py
+++ addons/lims_reception/models/lims_custody_chain.py
@@ -203,3 +203,31 @@ class LimsCustodyChain(models.Model):
             'target': 'new',
             'context': ctx,
         }
+    
+    def action_view_signed_document(self):
+        """Acción para ver/descargar el documento firmado"""
+        self.ensure_one()
+        
+        # Generar el PDF
+        report = self.env.ref('lims_reception.action_report_custody_chain')
+        pdf_content, content_type = report._render_qweb_pdf([self.id])
+        
+        # Crear attachment temporal
+        safe_code = self.custody_chain_code.replace('/', '_') if self.custody_chain_code else 'comprobante'
+        filename = f'{safe_code}.pdf'
+        
+        attachment = self.env['ir.attachment'].create({
+            'name': filename,
+            'type': 'binary',
+            'datas': base64.b64encode(pdf_content),
+            'res_model': self._name,
+            'res_id': self.id,
+            'mimetype': 'application/pdf',
+        })
+        
+        # Retornar acción para abrir el PDF
+        return {
+            'type': 'ir.actions.act_url',
+            'url': f'/web/content/{attachment.id}?download=true',
+            'target': 'new',
+        }
