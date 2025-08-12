32f9b4887dde3291275f7a205153b05dbc7ffd26
Omar Martinez <omar.martinez92@gmail.com>
2025-08-11 17:36:54 -0700

    Pulir detalles modulo de customers 1 debug 15

--- addons/lims_customer/models/lims_customer.py
+++ addons/lims_customer/models/lims_customer.py
@@ -5,7 +5,7 @@ class LimsCustomer(models.Model):
     _inherit = 'res.partner'
 
     is_lims_customer = fields.Boolean(string='Cliente LIMS', default=True)
-    client_code = fields.Char(string="Código del Cliente", required=True)
+    client_code = fields.Char(string="Código del Cliente")
 
     # Campo computado para ordenamiento numérico (CON STORE)
     client_code_sequence = fields.Integer(
