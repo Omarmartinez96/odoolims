3fe2ab1196211ebd245a4598ef30330895cd9d1c
Omar Martinez <omar.martinez92@gmail.com>
2025-08-17 22:46:40 -0700

    Se a;ade modulo de usuarios con pin de verificacion 6

--- addons/lims_analyst_config/__init__.py
+++ addons/lims_analyst_config/__init__.py
@@ -1,12 +1,8 @@
 from . import models
 from . import wizard
 
-def post_init_hook(cr, registry):
-    """Hook ejecutado después de la instalación"""
-    from odoo import api, SUPERUSER_ID
-    
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    
+def post_init_hook(env):
+    """Hook ejecutado después de la instalación"""    
     # Establecer PIN por defecto al analista demo
     demo_analyst = env.ref('lims_analyst_config.analyst_demo_admin', raise_if_not_found=False)
     if demo_analyst:
