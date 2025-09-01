2474228b53bd7ec6e412a2a43f8c971f5431fb98
Omar Martinez <omar.martinez92@gmail.com>
2025-08-31 21:08:29 -0700

    Ajuste para cargar medios de cultivo acorde al parametro 8

--- addons/lims_reception/models/lims_parameter_media_process.py
+++ addons/lims_reception/models/lims_parameter_media_process.py
@@ -1,9 +1,9 @@
-from odoo import models, fields, api
+from odoo import models, fields
 
 class LimsParameterMediaProcess(models.Model):
     _name = 'lims.parameter.media.process'
     _description = 'Medios de Cultivo por Proceso en Parámetros'
-    _order = 'sequence, process_type'
+    _order = 'sequence'
 
     sequence = fields.Integer(
         string='Secuencia', 
@@ -13,7 +13,6 @@ class LimsParameterMediaProcess(models.Model):
     parameter_id = fields.Many2one(
         'lims.sample.parameter',
         string='Parámetro',
-        required=True,
         ondelete='cascade'
     )
     
@@ -23,41 +22,18 @@ class LimsParameterMediaProcess(models.Model):
         ('quantitative', 'Análisis Cuantitativo'),
         ('qualitative', 'Análisis Cualitativo'),  
         ('confirmation', 'Confirmación')
-    ], string='Tipo de Proceso', required=True)
+    ], string='Tipo de Proceso', default='quantitative')
     
-    culture_media_id = fields.Many2one(
-        'lims.culture.media',
-        string='Medio de Cultivo',
-        required=True
+    culture_media_name = fields.Char(
+        string='Nombre del Medio'
     )
     
     media_usage = fields.Selection([
         ('diluyente', 'Diluyente'),
-        ('eluyente', 'Eluyente'),
         ('enriquecimiento', 'Enriquecimiento'),
         ('desarrollo_selectivo', 'Desarrollo Selectivo'),
-        ('desarrollo_diferencial', 'Desarrollo Diferencial'),
-        ('desarrollo_selectivo_diferencial', 'Desarrollo Selectivo y Diferencial'),
         ('pruebas_bioquimicas', 'Pruebas Bioquímicas'),
-        ('transporte', 'Transporte'),
-        ('mantenimiento', 'Mantenimiento'),
         ('otro', 'Otro')
-    ], string='Uso del Medio', required=True, default='diluyente')
+    ], string='Uso del Medio', default='diluyente')
     
-    notes = fields.Text(
-        string='Notas'
-    )
-    
-    # @api.onchange('process_type')
-    # def _onchange_process_type_default_usage(self):
-    #     """Establecer uso por defecto según el tipo de proceso"""
-    #     if self.process_type == 'pre_enrichment':
-    #         self.media_usage = 'enriquecimiento'
-    #     elif self.process_type == 'selective_enrichment':
-    #         self.media_usage = 'desarrollo_selectivo'
-    #     elif self.process_type == 'quantitative':
-    #         self.media_usage = 'diluyente'
-    #     elif self.process_type == 'qualitative':
-    #         self.media_usage = 'desarrollo_selectivo'
-    #     elif self.process_type == 'confirmation':
-    #         self.media_usage = 'pruebas_bioquimicas'
+    notes = fields.Text(string='Notas')
